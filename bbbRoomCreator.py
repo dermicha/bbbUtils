878e3d987d2f7bb6e6138612bf27a7d029167c0c
derMicha <dermicha@ubirch.com>
2020-09-24 12:41:59 +0200

    added roomId as a new parameter

--- bbbRoomCreator.py
+++ bbbRoomCreator.py
@@ -9,17 +9,17 @@ logging.info("bbbRoomUtil Example")
 
 parser = argparse.ArgumentParser(description='bbbRoomUtil Example')
 parser.add_argument('-u', dest='bbbUrl', metavar='BBBURL', required=True, help='https://bbb.yourserver.com/bigbluebutton/')
-parser.add_argument('-s', dest='bbbSec', metavar='BBBSEC', required=True, help='BigBlueButton API Secret', )
+parser.add_argument('-s', dest='bbbSec', metavar='BBBSEC', required=True, help='BigBlueButton API Secret')
+parser.add_argument('-r', dest='bbbRoom', metavar='BBBROOM', required=False, help='BigBlueButton Room Name', default=None)
 args = parser.parse_args()
 
 bbb = RoomUtil(args.bbbUrl, args.bbbSec)
 
-roomId = "room-{}".format(str(uuid.uuid4()))
+if (args.bbbRoom == None):
+    roomId = "room-{}".format(str(uuid.uuid4()))
+else:
+    roomId = args.bbbRoom
+
 userName = "user-{}".format(str(uuid.uuid4()))
 logging.info(bbb.getRoomUrl(roomId))
 logging.info(bbb.getRoomUrl(roomId, userName, moderator=True))
-
-# roomId = "room-{}".format(str(uuid.uuid4()))
-# userName = "user-{}".format(str(uuid.uuid4()))
-# logging.info(bbb.getRoomUrl(roomId))
-# logging.info(bbb.getRoomUrl(roomId, userName, moderator=True))
